d7c67403c0406f14591aa15baa74398db0ca47d0
thosewhoyoulove <2406001659@qq.com>
2022-12-17 00:21:28 +0800

    完成静态页面的编写

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,160 +3,160 @@
  * @Author: 曹俊
  * @Date: 2022-12-12 18:00:18
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-12-16 22:30:31
+ * @LastEditTime: 2022-12-16 22:45:34
 -->
 
 <template>
     <div class="flex-col w-100vw h-100vh pt-20">
-       <div class="text-sm">
-      <input class="bg-hex-ddd" id="document" type="file" />
-      <div  class="row">
-        <div class="span8">
-          <div id="output" class="well"></div>
+        <div class="text-sm">
+            <input class="bg-hex-ddd" id="document" type="file" />
+            <div class="row">
+                <div class="span8">
+                    <div id="output" class="well"></div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div @click="toParse" class="mt-5"><el-button type="primary">解析文件</el-button></div>
+        <div @click="toParse" class="mt-5"><el-button type="primary">解析文件</el-button></div>
     </div>
 
-  </template>
-  <script setup lang="ts">
-  import { useRouter, useRoute } from 'vue-router'
-  import { onMounted, reactive } from "vue";
-  const router = useRouter()
-  const route = useRoute()
-  const state  = reactive({
-    data:[]
-  })
-  const render = (content:any) => {
-      state.data = content
-  }
-  const toParse = () =>{
+</template>
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+import { onMounted, reactive } from "vue";
+const router = useRouter()
+const route = useRoute()
+const state = reactive({
+    data: []
+})
+const render = (content: any) => {
+    state.data = content
+}
+const toParse = () => {
     console.log('111');
-    
+
     router.push({
-      path:'/parse',
-      query:{
-        data:state.data
-      }
+        path: '/parse',
+        query: {
+            data: JSON.stringify(state.data)
+        }
     })
-  }
-  const main = () => {
+}
+const main = () => {
     let doc = document.getElementById("document") as any;
     doc.addEventListener("change", handleFileSelect, false);
-   
-      const containerDiv = document.getElementById('output') as any;
-      containerDiv.style.display = "none";
-   
-  
+
+    const containerDiv = document.getElementById('output') as any;
+    containerDiv.style.display = "none";
+
+
     function handleFileSelect(event: any) {
-      readFileInputEventAsArrayBuffer(event, function (arrayBuffer: any) {
-        mammoth.convertToHtml({ arrayBuffer: arrayBuffer }).then(displayResult);
-      });
+        readFileInputEventAsArrayBuffer(event, function (arrayBuffer: any) {
+            mammoth.convertToHtml({ arrayBuffer: arrayBuffer }).then(displayResult);
+        });
     }
-  
+
     function displayResult(result: any) {
-      // 获取id为output的表格元素
-      const outputElement = document.getElementById("output") as any;
-      outputElement.innerHTML = result.value;
-      const tableElement = outputElement.querySelector("table");
-      const tbodyElement = tableElement.querySelector("tbody");
-      console.log(tbodyElement, "tbodyElement");
-      // 获取表格中的每一行
-      const rows = tbodyElement.getElementsByTagName("tr");
-  
-      // 定义一个数组，用来保存表格中的内容
-      const tableData = [];
-  
-      for (let i = 0; i < rows.length; i++) {
-        // 获取每一行的所有单元格
-        const cells = rows[i].getElementsByTagName("td");
-  
-        // 定义一个数组，用来保存每一行的内容
-        const rowData = [];
-  
-        for (let j = 0; j < cells.length; j++) {
-          // 获取单元格中的内容
-          const cellContent = cells[j].children[0].innerText;
-  
-          // 将单元格中的内容添加到行数组中
-          rowData.push(cellContent);
+        // 获取id为output的表格元素
+        const outputElement = document.getElementById("output") as any;
+        outputElement.innerHTML = result.value;
+        const tableElement = outputElement.querySelector("table");
+        const tbodyElement = tableElement.querySelector("tbody");
+        console.log(tbodyElement, "tbodyElement");
+        // 获取表格中的每一行
+        const rows = tbodyElement.getElementsByTagName("tr");
+
+        // 定义一个数组，用来保存表格中的内容
+        const tableData = [];
+
+        for (let i = 0; i < rows.length; i++) {
+            // 获取每一行的所有单元格
+            const cells = rows[i].getElementsByTagName("td");
+
+            // 定义一个数组，用来保存每一行的内容
+            const rowData = [];
+
+            for (let j = 0; j < cells.length; j++) {
+                // 获取单元格中的内容
+                const cellContent = cells[j].children[0].innerText;
+
+                // 将单元格中的内容添加到行数组中
+                rowData.push(cellContent);
+            }
+
+            // 将行数组添加到总数组中
+            tableData.push(rowData);
         }
-  
-        // 将行数组添加到总数组中
-        tableData.push(rowData);
-      }
-  
-      // 输出表格中的内容
-      console.log(tableData, 'tempbo');
-      let temp = tableData.reduce((pre, cur) => {
-        if(cur.length === 7) {
-          return [...pre, {day: cur[0], children: [cur.slice(1)]}]
-        } else {
-          pre[pre.length - 1].children.push(cur)
-          return pre
-        }
-      }, [])
-    console.log(temp, 'ttt2')
-      temp.forEach(item => {
-        console.log('1111')
-        item.children = item.children.map(i => {
-          console.log('22222')
-          return {
-            time: i[0],
-            place: i[1],
-            todo: i[2],
-            host: i[3],
-            participants: i[4],
-            preparationUnit: i[5]
-          }
+
+        // 输出表格中的内容
+        console.log(tableData, 'tempbo');
+        let temp = tableData.reduce((pre, cur) => {
+            if (cur.length === 7) {
+                return [...pre, { day: cur[0], children: [cur.slice(1)] }]
+            } else {
+                pre[pre.length - 1].children.push(cur)
+                return pre
+            }
+        }, [])
+        console.log(temp, 'ttt2')
+        temp.forEach(item => {
+            console.log('1111')
+            item.children = item.children.map(i => {
+                console.log('22222')
+                return {
+                    time: i[0],
+                    place: i[1],
+                    todo: i[2],
+                    host: i[3],
+                    participants: i[4],
+                    preparationUnit: i[5]
+                }
+            })
         })
-      })
-      console.log(temp, 'temp')
-      
-      render(tableData);
-      var messageHtml = result.messages
-        .map(function (message: any) {
-          return (
-            '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>"
-          );
-        })
-        .join("");
-      let messages = document.getElementById("messages") as any;
-      messages.innerHTML = "<ul>" + messageHtml + "</ul>";
-      return tableData;
+        console.log(temp, 'temp')
+
+        render(temp);
+        var messageHtml = result.messages
+            .map(function (message: any) {
+                return (
+                    '<li class="' + message.type + '">' + escapeHtml(message.message) + "</li>"
+                );
+            })
+            .join("");
+        let messages = document.getElementById("messages") as any;
+        messages.innerHTML = "<ul>" + messageHtml + "</ul>";
+        return tableData;
     }
-  
+
     function readFileInputEventAsArrayBuffer(event: any, callback: any) {
-      var file = event.target.files[0];
-  
-      var reader = new FileReader();
-  
-      reader.onload = function (loadEvent: any) {
-        var arrayBuffer = loadEvent.target.result;
-        callback(arrayBuffer);
-      };
-  
-      reader.readAsArrayBuffer(file);
+        var file = event.target.files[0];
+
+        var reader = new FileReader();
+
+        reader.onload = function (loadEvent: any) {
+            var arrayBuffer = loadEvent.target.result;
+            callback(arrayBuffer);
+        };
+
+        reader.readAsArrayBuffer(file);
     }
-  
+
     function escapeHtml(value: any) {
-      return value
-        .replace(/&/g, "&amp;")
-        .replace(/"/g, "&quot;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
+        return value
+            .replace(/&/g, "&amp;")
+            .replace(/"/g, "&quot;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
     }
-  };
-  onMounted(() => {
+};
+onMounted(() => {
     main();
-  });
-  </script>
+});
+</script>
   
-  <style scoped>
-  #id{
+<style scoped>
+#id {
     color: aliceblue;
     font-size: .5rem;
-  }
-  </style>
+}
+</style>
   
--- src/pages/parse.vue
+++ src/pages/parse.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        这是parse
+    <div class="w-100vw h-100vh pt-5">
+
+        <div
+            class="flex w-20 h-20 bg-hex-5F9CEF border rounded-xl my-0 mx-auto text-hex-fff items-center justify-center">
+            day
+        </div>
+        <div class="flex dashed-line text-center"></div>
+        <div class=" border border-hex-5F9CEF mx-4 my-1">
+            <div class="flex w-100% bg-hex-5F9CEF h-10 text-center items-center justify-center text-hex-fff">todo</div>
+            <div class="text-xs p-3">
+                <div class="flex">time</div>
+                <div class="flex">place</div>
+            </div>
+
+            <div class="flex rectangle">
+                <div class="absolute w-0 h-0 triangle right-0"></div>
+                <div class="flex text-xs text-hex-fff items-center justify-center ml-1">相关信息</div>
+            </div>
+            <div class="text-xs p-3">
+                <div class="flex mb-2">主持人：host</div>
+                <div class="flex mb-2">准备单位：preparationUnit</div>
+                <div class="flex">参加单位或人员：participants</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,12 +30,34 @@
 import { useRouter, useRoute } from 'vue-router'
 import { onMounted, reactive } from "vue";
 const route = useRoute()
-let data = route.query.data
+let data = JSON.parse(route.query.data)
 onMounted(() => {
-    console.log(data,'传过来的数据')
+    console.log(data, '传过来的数据')
 })
 </script>
 
 <style scoped>
+.dashed-line {
+    width: 1px;
+    height: 5rem;
+    border-left: 3px dashed blue;
+    margin: 0 auto;
+}
 
+.rectangle {
+    width: 70px;
+    /* Set the width of the rectangle */
+    height: 30px;
+    /* Set the height of the rectangle */
+    background-color: #5F9CEF;
+    /* Set the background color of the rectangle */
+    position: relative;
+
+}
+
+.triangle {
+    border-width: 15px;
+    border-style: solid;
+    border-color: transparent white transparent transparent;
+}
 </style>
