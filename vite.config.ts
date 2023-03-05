8242b1eab1a52d0effd84b69086df8f1d94fceff
thosewhoyoulove <2406001659@qq.com>
2023-03-05 23:42:12 +0800

    配置打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@
  * @Author: 曹俊
  * @Date: 2022-12-12 18:00:18
  * @LastEditors: 曹俊
- * @LastEditTime: 2022-12-26 23:28:47
+ * @LastEditTime: 2023-03-05 23:41:11
  */
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -15,6 +16,12 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [vue(), WindiCSS(), Pages(),
   AutoImport({
     resolvers: [ElementPlusResolver()],
